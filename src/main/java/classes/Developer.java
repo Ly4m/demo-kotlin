c672f9d5af32dc8ea874f0655d292c61d004db41
William Leemans <w.leemans@kiabi.com>
2018-01-03 12:18:12 +0100

    :sparkles: : add empty files

---
+++ src/main/java/classes/Developer.java
@@ -0,0 +1,44 @@
+package classes;
+
+import java.util.List;
+
+public class Developer {
+
+    private Long id;
+    private String name;
+    private String firstName;
+    private List<Skill> skills;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+}
